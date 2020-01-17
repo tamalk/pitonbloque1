8aedfbaca805991466f82b507758cc38a242fe41
tamalk <57144439+tamalk@users.noreply.github.com>
2020-01-17 10:56:53 +0100

    Update act14.py

--- act14.py
+++ act14.py
@@ -1,1 +1,11 @@
-
+nota = raw_input()
+if (nota == " a"):
+    print("bueno")
+elif (nota == " b"):
+    print("regular")
+elif (nota == " c"):
+    print("malo")
+elif (nota == " d"):
+    print("pesimo")
+else:
+    print(" ")
