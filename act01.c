50af0ea7979b1ffc855068c67fe7cde0d7b285b1
Mario de Miguel <57144439+tamalk@users.noreply.github.com>
2020-04-13 01:02:52 +0200

    Create act01.c

---
+++ act01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int radio;
+  scanf("%d", &radio);
+  double longitud = 2 * 3.14 * radio;
+  int longi;
+  longi = longitud;
+  printf("%d", longi);
+  return 0;
+}
